2bc1f78c90fd6d12dca8f11cd67445035d440b6d
Ron Lucke <lucke@elan-ev.de>
2025-01-26 12:52:57 +0100

    load flow courses

--- src/components/flow/dialog/EditFlows.vue
+++ src/components/flow/dialog/EditFlows.vue
@@ -1,16 +1,60 @@
 <script setup>
-import { computed } from 'vue';
+import { computed, ref, watch } from 'vue';
 
 import { useContextStore } from '../../../stores/context';
-import StudipQuicksearch from './../../studip/StudipQuicksearch.vue';
+import { useCoursesStore } from '../../../stores/courses';
+import { useFlowsStore } from '../../../stores/flows';
+
+import StudipActionMenu from './../../studip/StudipActionMenu.vue';
 import StudipDialog from './../../studip/StudipDialog.vue';
+import StudipQuicksearch from './../../studip/StudipQuicksearch.vue';
 
 const contextStore = useContextStore();
+const coursesStore = useCoursesStore();
+const flowsStore = useFlowsStore();
 
 const emit = defineEmits(['update:open']);
+const props = defineProps({
+    open: {
+        type: Boolean,
+        required: true,
+    },
+});
 
 const courseSearch = computed(() => contextStore.courseSearch);
 const currentUnit = computed(() => contextStore.selectedUnit);
+
+const unitFlows = computed(() => {
+    return flowsStore.all
+        .filter((flow) => flow.source_unit.data.id === currentUnit.value?.id)
+        .map((flow) => ({
+            ...flow,
+            target_course: {
+                attributes: coursesStore.byId(flow.target_course.data.id),
+            },
+        }));
+});
+
+// Fetch courses for the current unit flows
+const fetchCourses = async () => {
+    if (!currentUnit.value) {
+        return;
+    }
+    const relevantFlows = flowsStore.all.filter((flow) => flow.source_unit.data.id === currentUnit.value.id);
+
+    for (const flow of relevantFlows) {
+        await coursesStore.fetchById(flow.target_course.data.id);
+    }
+};
+
+watch(
+    () => props.open,
+    async (newValue) => {
+        if (newValue) {
+            await fetchCourses();
+        }
+    }
+);
 
 const addCourse = (value) => {
     console.log(value);
@@ -26,25 +70,57 @@ const updateFlows = () => {
     emit('update:open', false);
 };
 
+const deleteFlow = (flow) => {
+    console.log('delete flow');
+    console.log(flow);
+};
 </script>
 
 <template>
     <StudipDialog
-        :height="780"
-        :width="500"
+        :height="600"
+        :width="800"
         :title="$gettext('Verteilung bearbeiten')"
-        confirm-class="accept"
-        :close-text="$gettext('Abbrechen')"
-        :confirm-text="$gettext('Speichern')"
+        :close-text="$gettext('Schließen')"
         :open="open"
         @update:open="updateOpen"
-        @confirm="updateFlows"
     >
         <template #dialogContent>
+            <table class="default">
+                <thead>
+                    <tr>
+                        <th>{{ $gettext('Veranstaltung') }}</th>
+                        <th>{{ $gettext('Status') }}</th>
+                        <th>{{ $gettext('letzte Aktualisierung') }}</th>
+                        <th>{{ $gettext('erstellt am') }}</th>
+                        <th>{{ $gettext('Aktion') }}</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="flow in unitFlows" :key="flow.id">
+                        <td>{{ flow.target_course.attributes?.title || '---' }}</td>
+                        <td>{{ flow.status }}</td>
+                        <td>{{ flow.chdate }}</td>
+                        <td>{{ flow.mkdate }}</td>
+                        <td class="actions">
+                            <StudipActionMenu
+                                :context="$gettext('Verteiltes Lernmaterial')"
+                                :items="[{ id: 1, label: $gettext('Löschen'), icon: 'trash', emit: 'delete' }]"
+                                @delete="deleteFlow(flow)"
+                            />
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
             <form class="default cw-flow-dialog-edit">
                 <label>
-                    {{ $gettext('Veranstaltungssuche') }}
-                    <StudipQuicksearch :searchtype="courseSearch" name="qs" @select="addCourse" :placeholder="$gettext('Suchen')"></StudipQuicksearch>
+                    {{ $gettext('Veranstaltunghinzufügen') }}
+                    <StudipQuicksearch
+                        :searchtype="courseSearch"
+                        name="qs"
+                        @select="addCourse"
+                        :placeholder="$gettext('Suchen')"
+                    ></StudipQuicksearch>
                 </label>
             </form>
         </template>
