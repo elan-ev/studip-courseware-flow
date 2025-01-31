87f0da1d54464be380a3013f6d6d3dc413eb65c3
Ron Lucke <lucke@elan-ev.de>
2025-01-31 16:27:58 +0100

    sync containers and block quantity

--- lib/Helpers/CopyHelper.php
+++ lib/Helpers/CopyHelper.php
@@ -2,6 +2,7 @@
 
 namespace CoursewareFlow\Helpers;
 
+use \Courseware\Block;
 use \Courseware\Container;
 use \Courseware\StructuralElement;
 use \Courseware\Unit;
@@ -105,6 +106,20 @@ class CopyHelper
         self::copyContainers($user, $target_unit_structural_element, $source_unit_structural_element, $container_map, $blocks_map, $files_map, $folders_map);
         self::copyChildren($user, $target_unit_structural_element, $source_unit_structural_element, $structural_elements_map, $structural_elements_image_map, $container_map, $blocks_map, $files_map, $folders_map);
 
+        // rename filesystem folder
+        $parent_folder = null;
+        if (sizeof($files_map) > 0) {
+            $first_file = array_key_first($files_map);
+            $parent_folder = \Folder::find($files_map[$first_file])->getParent();
+        }
+        if (!$parent_folder  && sizeof($folders_map) > 0) {
+            $first_folder = array_key_first($folders_map);
+            $parent_folder = \Folder::find($folders_map[$first_folder])->getParent();
+        }
+
+        $parent_folder->__set('name',  str_replace('🔄 ', $source_unit_structural_element->title));
+        $parent_folder->store();
+
         return [
             'target_unit_structural_element' => $target_unit_structural_element,
             'structural_elements_map' => $structural_elements_map,
@@ -184,24 +199,27 @@ class CopyHelper
         ];
     }
 
-    protected static function copyContainers($user, $target_element, $source_element, &$container_map, &$blocks_map, &$files_map, &$folders_map): array
+    protected static function copyContainers($user, $target_element, $source_element, &$container_map, &$blocks_map, &$files_map, &$folders_map): void
     {
         foreach ($source_element->containers as $container) {
-            $new_container = Container::create([
-                'structural_element_id' => $target_element->id,
-                'owner_id' => $user->id,
-                'editor_id' => $user->id,
-                'edit_blocker_id' => null,
-                'position' => $container->position,
-                'container_type' => $container->type->getType(),
-                'payload' => $container->payload,
-            ]);
-
-            addToMap($container_map, $container->id, $new_container->id);
-            self::copyBlocks($user, $new_container, $container, $blocks_map, $files_map, $folders_map);
+            self::copyContainer($user, $target_element, $container, $container_map, $blocks_map, $files_map, $folders_map);
         }
+    }
 
-        return [$container_map, $blocks_map];
+    public static function copyContainer($user, $target_element, $source_container, &$container_map, &$blocks_map, &$files_map, &$folders_map): void
+    {
+        $new_container = Container::create([
+            'structural_element_id' => $target_element->id,
+            'owner_id' => $user->id,
+            'editor_id' => $user->id,
+            'edit_blocker_id' => null,
+            'position' => $source_container->position,
+            'container_type' => $source_container->type->getType(),
+            'payload' => $source_container->payload,
+        ]);
+
+        addToMap($container_map, $source_container->id, $new_container->id);
+        self::copyBlocks($user, $new_container, $source_container, $blocks_map, $files_map, $folders_map);
     }
 
     protected static function copyBlocks($user, $target_container, $source_container, &$blocks_map, &$files_map, &$folders_map): array
@@ -219,6 +237,15 @@ class CopyHelper
         return [$blocks_map, $newBlockList];
     }
 
+    public static function copyBlock($user, $target_container, $source_block, &$blocks_map, &$files_map, &$folders_map): Block
+    {
+        $newBlock = $source_block->copy($user, $target_container);
+        self::mapFiles($files_map, $newBlock, $source_block);
+        self::mapFolders($folders_map, $newBlock, $source_block);
+        addToMap($blocks_map, $source_block->id, $newBlock->id);
+
+        return $newBlock;
+    }
     protected static function mapFiles(&$files_map, $target_block, $source_block): void
     {
         $source_payload = $source_block->type->getPayload();
--- lib/Helpers/SyncHelper.php
+++ lib/Helpers/SyncHelper.php
@@ -2,6 +2,7 @@
 
 namespace CoursewareFlow\Helpers;
 
+use \Courseware\Block;
 use \Courseware\Container;
 use \Courseware\StructuralElement;
 use \Courseware\Unit;
@@ -36,7 +37,7 @@ class SyncHelper
                 $target_unit->store();
             }
             self::syncStructuralElementsQuantity($flow, $source_unit->structural_element, $user);
-            self::syncStructuralElements($flow, $target_unit->structural_element , $source_unit->structural_element);
+            self::syncStructuralElements($flow, $target_unit->structural_element , $source_unit->structural_element, $user);
         } else {
             $target = CopyHelper::copyUnit($user, $source_unit, $flow->target_course_id);
             $flow->target_unit_id = $target['target_unit']->id;
@@ -89,7 +90,7 @@ class SyncHelper
         }
     }
 
-    private static function syncStructuralElements(Flow &$flow, StructuralElement $target_element, StructuralElement $source_element): void
+    private static function syncStructuralElements(Flow &$flow, StructuralElement $target_element, StructuralElement $source_element, $user): void
     {
         $has_changes = false;
 
@@ -121,8 +122,8 @@ class SyncHelper
 
         //todo: image_id && image_ref
         //todo: sync containers
-        self::syncContainers($flow, $source_element, $target_element);
-
+        self::syncContainers($flow, $source_element, $target_element, $user);
+die();
 
         if ($has_changes) {
             $target_element->store();
@@ -136,13 +137,110 @@ class SyncHelper
         }
     }
 
-    private static function syncContainers(&$flow, $source_element, $target_element): void
+    private static function syncContainers(&$flow, $source_element, $target_element, $user): void
     {
-        // $source_element->containers
-        // $target_element->containers
+        $deletable_containers = array_map($target_element->containers, 'id');
+
+        self::syncBlocksQuantity($flow, $source_element, $target_element, $user);
+
+        foreach ($source_element->containers as $container) {
+            $target_container = Container::find($flow->container_map[$container->id]);
+            if ($target_container) { 
+                unset($deletable_containers, $target_container->id);
+                self::syncContainerPayload($flow, $target_container, $container, $user);
+                // todo: self::syncBlocks();
+            } else {
+                CopyHelper::copyContainer($user, $target_element, $container, $flow->container_map, $flow->blocks_map, $flow->files_map, $flow->folders_map);
+                $flow->store();
+            }
+        }
+        
+        if (sizeof($deletable_containers) > 0) {
+            foreach ($deletable_containers as $container_id) {
+                $container = Container::find($container_id);
+                if ($container) {
+                    $container->delete();
+                }
+            }
+        }
     }
 
-    private static function syncBlocks($source_course_id, $target_course_id): void
+    private static function syncBlocksQuantity(Flow &$flow, StructuralElement $source_element, StructuralElement $target_element, $user): void
+    {
+        $source_containers = $source_element->containers;
+        $target_containers = $target_element->containers;
+
+        $blocks_map = json_decode($flow->blocks_map, true);
+        $files_map = json_decode($flow->files_map, true);
+        $folders_map = json_decode($flow->folders_map, true);
+        $map_changed = false;
+
+        $source_blocks = [];
+        $target_blocks = [];
+        foreach ($source_containers as $source_container) {
+            $source_blocks = array_merge($source_blocks,  Block::findBySQL('container_id = ?', [$source_container->id]));
+        }
+        foreach ($target_containers as $target_container) {
+            $target_blocks = array_merge($target_blocks,  Block::findBySQL('container_id = ?', [$target_container->id]));
+        }
+        $deletable_blocks = array_column($target_blocks, 'id');
+
+        foreach($source_blocks as $block) {
+            $target_block = $flow->blocks_map[$block->id];
+            
+            if ($target_block) {
+                $deletable_blocks = array_diff($deletable_blocks, [$target_block]);
+            } else {
+                $target_container = Container::find($flow->container_map[$block->container_id]);
+
+                CopyHelper::copyBlock($user, $target_container, $block,  $blocks_map, $files_map, $folders_map);
+                $map_changed = true;
+            }
+        }
+
+        if ($map_changed) {
+            $flow->blocks_map = json_encode($blocks_map);
+            $flow->files_map = json_encode($files_map);
+            $flow->folders_map = json_encode($folders_map);
+            $flow->store();
+        }
+
+        foreach ($deletable_blocks as $block_id) {
+            $block = Block::find($block_id);
+            if ($block) {
+                $block->delete();
+            }
+        }
+
+    }
+
+    private static function syncContainerPayload(Flow $flow, Container $target_container, Container $source_container, $user): void
+    {
+        $source_payload = json_decode($source_container->payload, true);
+        $target_payload = json_decode($target_container->payload, true);
+        // sync settings
+        $target_payload['colspan'] = $source_payload['colspan'];
+        
+        // sync sections use block_map
+        foreach ($source_payload['sections'] as $index => $section) {
+            $target_payload['sections'][$index]['name'] = $section['name'];
+            $target_payload['sections'][$index]['icon'] = $section['icon'];
+
+            //todo: sync block ids and sort use block_map
+            $target_block_list = [];
+            foreach ($section['blocks'] as $block_id) {
+                $target_block = Block::find($flow->blocks_map[$block_id]);
+                if ($target_block) {
+                    $target_block_list[] = $target_block->id;
+                }
+            }
+            $target_payload['sections'][$index]['blocks'] = $target_block_list;
+        }
+
+        // store target container
+    }
+
+    private static function syncBlocks(): void
     {
 
     }
