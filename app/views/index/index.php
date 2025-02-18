3b69238fc139f04f6a4967faeed2612f85f88131
Ron Lucke <lucke@elan-ev.de>
2025-02-18 16:06:56 +0100

    for members only

--- app/views/index/index.php
+++ app/views/index/index.php
@@ -1,6 +1,10 @@
+<? if ($isTeacher): ?>
 <div
     id="courseware-flow-app"
     is-teacher='<?= var_export($isTeacher) ?>'
     preferred-language='<?= $preferredLanguage ?>'
     course-search='<?= $courseSearch ?>'
 ></div>
+<? else: ?>
+    <?= MessageBox::info(sprintf( _('Mit Ihrer Rechtestufe steht Ihnen dieses Werkzeug nicht zur Verfügung.'))) ?>
+<? endif ?>
