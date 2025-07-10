6cb9ba57e35919f902c5ba6ff3826f40f585fd1e
Ron Lucke <lucke@elan-ev.de>
2025-07-10 17:05:47 +0200

    add helpbar content

--- app/controllers/index.php
+++ app/controllers/index.php
@@ -11,6 +11,13 @@ class IndexController extends StudipController
     {
         global $perm, $user;
 
+        $helpbar = Helpbar::get();
+        $helpbar->addPlainText('', 
+            _('Mit CoursewareFlow können Sie Lernmaterialien in andere Veranstaltungen Verteilen. Die Lernmaterialien werden entweder automatisch oder manuell mit dem Quellmaterial synchronisiert. Bei der Verteilung werden sowohl die Dateien aus dem Dateibereich, also auch Vips Aufgaben und OpenCast Videos in die Zielveranstaltung übertragen.')
+        );
+        $helpbar->addPlainText('',  _('Dieses Stud.IP Plugin wurde vom elan e.V. entwickelt.'));
+        $helpbar->addLink('GitHub Repository', 'https://github.com/elan-ev/studip-courseware-flow', Icon::create('link-extern', Icon::ROLE_INFO_ALT), '_blank');
+
         if (Navigation::hasItem('course/coursewareflow')) {
             Navigation::activateItem('course/coursewareflow/index');
             PageLayout::setBodyElementId('coursewareflow-index');
